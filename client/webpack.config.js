e5b706e9785887ae6c488b82ee3551799cfda194
dcrlsn <c4r150n@gmail.com>
2022-01-26 20:28:26 -0500

    database logic working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,24 +11,61 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      header: './src/js/header.js',
+      editor: './src/js/editor.js',
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: '[name].js',
       path: path.resolve(__dirname, 'dist'),
+      publicPath: '',
     },
     plugins: [
       new HtmlWebpackPlugin({
-        {
-          title; 'Text Editor',
-        }
+        title: 'Text Editor',
+        template: './index.html'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: "Class App",
+        short_name: "Class App",
+        description: "Class App",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        start_url: "/",
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }
+        ]
       })
     ],
 
-  module: {
-    rules: [
-
-    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
+      ]
     },
-};
+  };
 };
